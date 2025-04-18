9ed0cc4d90a4086649f4909e26d075b7952ed3c3
Hannan Dev <hannandeveloper1@gmail.com>
2025-04-18 21:10:34 +0500

    Initial Base

---
+++ apps/auth-service/src/main.ts
@@ -0,0 +1,25 @@
+import express from 'express';
+import cors from 'cors'
+
+
+const app = express();
+
+app.use(cors({
+  origin: ['http://localhost:3000'],
+  allowedHeaders:['Authorization', 'Content-Type'],
+  credentials: true
+}));
+
+app.get('/', (req, res) => {
+    res.send({ 'message': 'Hello API'});
+});
+
+const port = process.env.PORT ? Number(process.env.PORT) : 6001;
+
+const server = app.listen(port, () => {
+    console.log(`Auth service is running on http://localhost:${port}/api`);
+});
+
+server.on('error', (err)=>{
+  console.log("Server Error", err)
+});
